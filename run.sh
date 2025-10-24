9f930511467311ce0f8347ed5c474059c5a05d34
Sebastian Götz <sebastian.goetz1@tu-dresden.de>
2025-10-24 11:00:32 +0200

    fixes

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker run -it --net=host --ipc=host --privileged \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="${XAUTHORITY}:/root/.Xauthority" \
     seams31/seams25-paper31:crom-fchain \
-    bash -c "cd ros_ws && source install/setup.bash && argos3 -c bridge_example.argos & cd ../Context-Role-Oriented-Foraging-Chain-Swarm && python3 startup/automatedStartup.py"
+    bash -c "cd ros_ws && source install/setup.bash && argos3 -c bridge_example.argos && cd ../Context-Role-Oriented-Foraging-Chain-Swarm/startup && source bin/activate && python3 startup/automatedStartup.py"
